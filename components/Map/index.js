c62f0d124d2df59616f1ba9d70f9605bd2ae15a6
David <daviddamilola20@gmail.com>
2021-11-13 13:53:40 +0100

    chore(map):set up deckgl with mapbox

---
+++ components/Map/index.js
@@ -0,0 +1,37 @@
+import React from "react";
+import DeckGL from "@deck.gl/react";
+import { LineLayer } from "@deck.gl/layers";
+import { StaticMap } from "react-map-gl";
+
+// Viewport settings
+const INITIAL_VIEW_STATE = {
+  longitude: -122.41669,
+  latitude: 37.7853,
+  zoom: 13,
+  pitch: 0,
+  bearing: 0,
+};
+
+// Data to be used by the LineLayer
+const data = [
+  { sourcePosition: [9.082, 8.6753], targetPosition: [9.082, 8.6753] },
+];
+
+// DeckGL react component
+function MapExplorer() {
+  const layers = [new LineLayer({ id: "line-layer", data })];
+  return (
+    <DeckGL
+      initialViewState={INITIAL_VIEW_STATE}
+      controller={true}
+      layers={layers}
+    >
+      <StaticMap
+        mapStyle="mapbox://styles/mapbox/dark-v9"
+        mapboxApiAccessToken={process.env.NEXT_PUBLIC_MAPBOX_TOKEN}
+      />
+    </DeckGL>
+  );
+}
+
+export default MapExplorer;
