5dfa20350e6bb29ac5eda7823e3d8d89e4f1a7b7
David <daviddamilola20@gmail.com>
2021-11-13 13:55:47 +0100

    chore(map): add nigerias coordinates and change zoom level

--- components/Map/index.js
+++ components/Map/index.js
@@ -5,9 +5,9 @@ import { StaticMap } from "react-map-gl";
 
 // Viewport settings
 const INITIAL_VIEW_STATE = {
-  longitude: -122.41669,
-  latitude: 37.7853,
-  zoom: 13,
+  longitude: 9.082,
+  latitude: 8.6753,
+  zoom: 5,
   pitch: 0,
   bearing: 0,
 };
